113eeeb56a3587d979b72ead6dde95d623dc6827
silvaadriel <silvaadriel@hotmail.com>
2019-02-18 12:25:08 -0300

    function expression changed to function declaration

--- js/numbersToBeSorted.js
+++ js/numbersToBeSorted.js
@@ -1,1 +1,3 @@
-export default numberToBeSorted = () => [9, 2, 1, 0, 5, 7, 8, 3, 4, 6, 10, 12, 11];
+export default function numberToBeSorted() {
+  return [9, 2, 1, 0, 5, 7, 8, 3, 4, 6, 10, 12, 11];
+};
