a522f0a812579e00b02b8991c62b0c9d4934cae3
matheus leite <matheus.leite.san@gmail.com>
2019-02-24 16:06:37 -0300

    creating actions for the buttons

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ import startSorting from './sort.js';
 import { getNumbersToBeSorted, setNumbersToBeSorted } from './numbersToBeSorted.js'
 import { Button } from './controls.js';
 import arrayStructure from './arrayStructure.js';
+import { hiddenDisplayInput } from './hiddenDisplayInput.js';
 
-setNumbersToBeSorted();
+const ARRAY_TO_BE_SORTED = [];
+
+setNumbersToBeSorted(ARRAY_TO_BE_SORTED);
 
 const playbackStatus = {
   isPaused: false
@@ -46,3 +49,25 @@ btnRandom.onClick(() => {
 });
 
 arrayStructure.render(getNumbersToBeSorted());
+
+const disableButton = document.querySelector('button[name=btnDisabled]');
+disableButton.onclick = () => {
+  const input = document.querySelector('.input');
+  hiddenDisplayInput(input);
+};
+
+const clearArray = new Button('#btnClear');
+clearArray.onClick(() => {
+  arrayStructure.render([]);
+});
+
+const addButton = document.querySelector('button[name=btnAddArray]');
+addButton.onclick = () => {
+  const inputValues = document.querySelector('input[name=inputValues]');
+  inputValues.value === ''
+    ? alert('You need to input a number')
+    : ARRAY_TO_BE_SORTED.push(inputValues.value);
+  setNumbersToBeSorted(ARRAY_TO_BE_SORTED);
+  arrayStructure.render(ARRAY_TO_BE_SORTED);
+  inputValues.value = '';
+};
